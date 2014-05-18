5d0368ce06ae1b810e27b81b38251be9703b4c75
Anubhav <anubhav200@gmail.com>
2014-05-19 00:27:46 +0530

    Release Version 0.2.3
    Removed Comments.

--- dist/0.2.3/PowerJS.js
+++ dist/0.2.3/PowerJS.js
@@ -199,32 +199,6 @@ function createNamespace(scope,index,strArray){
         };
 
 
-
-    /*
-
-        ^
-        =
-        =
-        =
-        |
-        ^
-
-     modules ={
-
-             com:{
-                 google:{
-                           __pjs__:{
-                               $classData:{},
-                               $injectable:{}
-                           }
-                           class1: function(){};
-                           class2: function(){};
-                       }
-                 }
-     };
-    * */
-
-
     /**
      * Return complete model structure.
      *
@@ -246,7 +220,6 @@ function createNamespace(scope,index,strArray){
      * @returns {*}
      */
     window.module = function(namespaceStr,scope){
-
         var retModule;
         if(namespaceStr === undefined && scope === undefined){      // for anonymous Classes
             retModule = new JSModule();
@@ -269,81 +242,5 @@ function createNamespace(scope,index,strArray){
 
         return retModule;
     };
-
-
-/*
- // ======================================================SOME IDEAS==============================================================================
-var model       =   module("GRID.model");
-var view        =   module("GRID.view");
-var controller  =   module("GRID.controller");
-
-
-model.$Class("Employee",function(){
-
-    var employeeList = [];
-
-    this.addEmployee = function(){};
-
-    this.showAllEmployees = function(){};
-
-    this.deleteAllemployees = function(){}
-
-});
-
-model.$Injectable("$Employee",{
-    type:"new",//singleton,reference
-    injectables:["$List,$Management"],
-    params:[]
-});
-
-model.$Class
-    .extend("SIMS.access.grid.Class1")
-    .$Inject([])
-    .define("name",function(){
-
-    });
-
-model.$Class.define("className",function(){
-    this.method = function(){
-
-        //alternate way to define classes/
-
-    }
-});
-
-*/
-/*javascript bean classes......*//*
-
-
-
-model.$Class
-    .$Annotate(test="sdasdasd")
-    .$Implements("NJS.Bean")
-    .define("beanName", function () {
-        var
-            name,
-            place;
-
-        this.getName = function (name) {
-            return name;
-        };
-
-        this.setName = function () {
-            name = name;
-        };
-
-        this.setPlace = function (place) {
-            place = place;
-        };
-
-        this.getPlace = function () {
-            return place;
-        }
-
-    });
-
- // ======================================================SOME IDEAS==============================================================================
-
-*/
 })(window)
 ;
--- src/core.js
+++ src/core.js
@@ -10,32 +10,6 @@
         };
 
 
-
-    /*
-
-        ^
-        =
-        =
-        =
-        |
-        ^
-
-     modules ={
-
-             com:{
-                 google:{
-                           __pjs__:{
-                               $classData:{},
-                               $injectable:{}
-                           }
-                           class1: function(){};
-                           class2: function(){};
-                       }
-                 }
-     };
-    * */
-
-
     /**
      * Return complete model structure.
      *
@@ -57,7 +31,6 @@
      * @returns {*}
      */
     window.module = function(namespaceStr,scope){
-
         var retModule;
         if(namespaceStr === undefined && scope === undefined){      // for anonymous Classes
             retModule = new JSModule();
@@ -80,79 +53,3 @@
 
         return retModule;
     };
-
-
-/*
- // ======================================================SOME IDEAS==============================================================================
-var model       =   module("GRID.model");
-var view        =   module("GRID.view");
-var controller  =   module("GRID.controller");
-
-
-model.$Class("Employee",function(){
-
-    var employeeList = [];
-
-    this.addEmployee = function(){};
-
-    this.showAllEmployees = function(){};
-
-    this.deleteAllemployees = function(){}
-
-});
-
-model.$Injectable("$Employee",{
-    type:"new",//singleton,reference
-    injectables:["$List,$Management"],
-    params:[]
-});
-
-model.$Class
-    .extend("SIMS.access.grid.Class1")
-    .$Inject([])
-    .define("name",function(){
-
-    });
-
-model.$Class.define("className",function(){
-    this.method = function(){
-
-        //alternate way to define classes/
-
-    }
-});
-
-*/
-/*javascript bean classes......*//*
-
-
-
-model.$Class
-    .$Annotate(test="sdasdasd")
-    .$Implements("NJS.Bean")
-    .define("beanName", function () {
-        var
-            name,
-            place;
-
-        this.getName = function (name) {
-            return name;
-        };
-
-        this.setName = function () {
-            name = name;
-        };
-
-        this.setPlace = function (place) {
-            place = place;
-        };
-
-        this.getPlace = function () {
-            return place;
-        }
-
-    });
-
- // ======================================================SOME IDEAS==============================================================================
-
-*/
